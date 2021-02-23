474573ae4a3c2017c4b2dd1535839006917e0e99
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2021-02-23 17:00:43 +0300

    Add hw2

--- src/phg/matching/flann_matcher.cpp
+++ src/phg/matching/flann_matcher.cpp
@@ -6,8 +6,8 @@
 phg::FlannMatcher::FlannMatcher()
 {
     // параметры для приближенного поиска
-//    index_params = flannKdTreeIndexParams(TODO);
-//    search_params = flannKsTreeSearchParams(TODO);
+    index_params = flannKdTreeIndexParams();
+    search_params = flannKsTreeSearchParams();
 }
 
 void phg::FlannMatcher::train(const cv::Mat &train_desc)
@@ -17,5 +17,18 @@ void phg::FlannMatcher::train(const cv::Mat &train_desc)
 
 void phg::FlannMatcher::knnMatch(const cv::Mat &query_desc, std::vector<std::vector<cv::DMatch>> &matches, int k) const
 {
-    throw std::runtime_error("not implemented yet");
+    size_t n_desc = query_desc.rows;
+    cv::Mat indices(n_desc, k, CV_32SC1);
+    cv::Mat distances2(n_desc, k, CV_32FC1);
+
+    flann_index->knnSearch(query_desc, indices, distances2, k, *search_params);
+
+    matches.resize(n_desc, std::vector<cv::DMatch>(k));
+    for (int i = 0; i < n_desc; ++i) {
+        for (int j = 0; j < k; ++j) {
+            matches[i][j].distance = distances2.at<float>(i, j);
+            matches[i][j].queryIdx = i;
+            matches[i][j].trainIdx = indices.at<int>(i, j);
+        }
+    }
 }
