52778dc0be84746bbe0132c493df3446a2d02081
Michael19 <socolovmisha@mail.ru>
2021-03-01 15:14:50 +0300

    FmatrixSimple test complete

--- src/phg/sfm/fmatrix.cpp
+++ src/phg/sfm/fmatrix.cpp
@@ -8,170 +8,199 @@
 
 namespace {
 
-    void infoF(const cv::Matx33d &Fcv)
-    {
-        Eigen::MatrixXd F;
-        copy(Fcv, F);
+void infoF(const cv::Matx33d &Fcv)
+{
+    Eigen::MatrixXd F;
+    copy(Fcv, F);
 
-        Eigen::JacobiSVD<Eigen::MatrixXd> svdf(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::JacobiSVD<Eigen::MatrixXd> svdf(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
-        Eigen::MatrixXd U = svdf.matrixU();
-        Eigen::VectorXd s = svdf.singularValues();
-        Eigen::MatrixXd V = svdf.matrixV();
+    Eigen::MatrixXd U = svdf.matrixU();
+    Eigen::VectorXd s = svdf.singularValues();
+    Eigen::MatrixXd V = svdf.matrixV();
 
-        std::cout << "F info:\nF:\n" << F << "\nU:\n" << U << "\ns:\n" << s << "\nV:\n" << V << std::endl;
+    std::cout << "F info:\nF:\n" << F << "\nU:\n" << U << "\ns:\n" << s << "\nV:\n" << V << std::endl;
+}
+
+// (см. Hartley & Zisserman p.279)
+cv::Matx33d estimateFMatrixDLT(const cv::Vec2d *m0, const cv::Vec2d *m1, int count)
+{
+    //        throw std::runtime_error("not implemented yet");
+    int a_rows = count;
+    int a_cols = 9;
+
+    Eigen::MatrixXd A(a_rows, a_cols);
+
+    for (int i_pair = 0; i_pair < count; ++i_pair) {
+
+        double x0 = m0[i_pair][0];
+        double y0 = m0[i_pair][1];
+
+        double x1 = m1[i_pair][0];
+        double y1 = m1[i_pair][1];
+        A.row(i_pair) << x0 * x1, x1 * y0, x1, y1 * x0, y1*y0,y1,x0,y0,1.;
+
+        //            std::cout << "(" << x0 << ", " << y0 << "), (" << x1 << ", " << y1 << ")" << std::endl;
+
+        //TODO
     }
 
-    // (см. Hartley & Zisserman p.279)
-    cv::Matx33d estimateFMatrixDLT(const cv::Vec2d *m0, const cv::Vec2d *m1, int count)
-    {
-        throw std::runtime_error("not implemented yet");
-//        int a_rows = TODO;
-//        int a_cols = TODO;
-//
-//        Eigen::MatrixXd A(a_rows, a_cols);
-//
-//        for (int i_pair = 0; i_pair < count; ++i_pair) {
-//
-//            double x0 = m0[i_pair][0];
-//            double y0 = m0[i_pair][1];
-//
-//            double x1 = m1[i_pair][0];
-//            double y1 = m1[i_pair][1];
-//
-////            std::cout << "(" << x0 << ", " << y0 << "), (" << x1 << ", " << y1 << ")" << std::endl;
-//
-//            TODO
-//        }
-//
-//        Eigen::JacobiSVD<Eigen::MatrixXd> svda(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
-//        Eigen::VectorXd null_space = TODO
-//
-//        Eigen::MatrixXd F(3, 3);
-//        F.row(0) << null_space[0], null_space[1], null_space[2];
-//        F.row(1) << null_space[3], null_space[4], null_space[5];
-//        F.row(2) << null_space[6], null_space[7], null_space[8];
-//
-////             Поправить F так, чтобы соблюдалось свойство фундаментальной матрицы (последнее сингулярное значение = 0)
-//        Eigen::JacobiSVD<Eigen::MatrixXd> svdf(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
-//
-//          TODO
-//
-//        cv::Matx33d Fcv;
-//        copy(F, Fcv);
-//
-//        return Fcv;
+    Eigen::JacobiSVD<Eigen::MatrixXd> svda(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::VectorXd null_space = svda.matrixV().col(svda.matrixV().cols()-1);
+
+    //    std::cout << " null_space " << A * null_space << std::endl;
+
+    Eigen::MatrixXd F(3, 3);
+    F.row(0) << null_space[0], null_space[1], null_space[2];
+    F.row(1) << null_space[3], null_space[4], null_space[5];
+    F.row(2) << null_space[6], null_space[7], null_space[8];
+    //
+    ////             Поправить F так, чтобы соблюдалось свойство фундаментальной матрицы (последнее сингулярное значение = 0)
+    Eigen::JacobiSVD<Eigen::MatrixXd> svdf(F, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+     // 280 Constraint enforcement
+    Eigen::Matrix3d D = Eigen::Matrix3d::Identity();
+    D(0,0) = svdf.singularValues()(0);
+    D(1,1) = svdf.singularValues()(1);
+    D(2,2) =0.;
+    F = svdf.matrixU() *(D * svdf.matrixV().transpose());
+    //
+    //          TODO
+    //
+    cv::Matx33d Fcv;
+    copy(F, Fcv);
+
+    return Fcv;
+}
+
+// Нужно создать матрицу преобразования, которая сдвинет переданное множество точек так, что центр масс перейдет в ноль, а Root Mean Square расстояние до него станет sqrt(2)
+// (см. Hartley & Zisserman p.107 Why is normalization essential?)
+cv::Matx33d getNormalizeTransform(const std::vector<cv::Vec2d> &m)
+{
+    cv::Vec2d center_mass;
+    /*находим центр масс, для каждой из точек ищем расстояние до центра масс*/
+
+    cv::Matx33d nt33 = cv::Matx33d::eye();
+    size_t count = m.size();
+    for(const cv::Vec2d& v : m){
+
+        nt33(0,2) += (v(0) / count);
+        nt33(1,2) += (v(1) / count);
+
+
+    }
+    double dist = 0;
+    for(const cv::Vec2d& v : m){
+        dist += (std::sqrt(std::pow(v(0) - nt33(0,2),2) + std::pow(v(1) - nt33(1,2),2)) / count);
     }
 
-    // Нужно создать матрицу преобразования, которая сдвинет переданное множество точек так, что центр масс перейдет в ноль, а Root Mean Square расстояние до него станет sqrt(2)
-    // (см. Hartley & Zisserman p.107 Why is normalization essential?)
-    cv::Matx33d getNormalizeTransform(const std::vector<cv::Vec2d> &m)
+    nt33(0,2) *= (-std::sqrt(2) / dist);
+    nt33(1,2) *= (-std::sqrt(2) / dist);
+
+    nt33(0,0) = std::sqrt(2) / dist;
+    nt33(1,1) = std::sqrt(2) / dist;
+
+    // Root Mean Square - ?
+
+    std::cout<< nt33 << std::endl;
+
+    //        throw std::runtime_error("not implemented yet");
+    return nt33;
+}
+
+cv::Vec2d transformPoint(const cv::Vec2d &pt, const cv::Matx33d &T)
+{
+    cv::Vec3d tmp = T * cv::Vec3d(pt[0], pt[1], 1.0);
+
+    if (tmp[2] == 0) {
+        throw std::runtime_error("infinite point");
+    }
+
+    return cv::Vec2d(tmp[0] / tmp[2], tmp[1] / tmp[2]);
+}
+
+cv::Matx33d estimateFMatrixRANSAC(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px)
+{
+    if (m0.size() != m1.size()) {
+        throw std::runtime_error("estimateFMatrixRANSAC: m0.size() != m1.size()");
+    }
+
+    const int n_matches = m0.size();
+
+    cv::Matx33d TN0 = getNormalizeTransform(m0);
+    cv::Matx33d TN1 = getNormalizeTransform(m1);
+
+    std::vector<cv::Vec2d> m0_t(n_matches);
+    std::vector<cv::Vec2d> m1_t(n_matches);
+    for (int i = 0; i < n_matches; ++i) {
+        m0_t[i] = transformPoint(m0[i], TN0);
+        m1_t[i] = transformPoint(m1[i], TN1);
+    }
+
     {
-        cv::Vec2d center_mass;
+        //             Проверьте лог: при повторной нормализации должно найтись почти единичное преобразование
+        getNormalizeTransform(m0_t);
+        getNormalizeTransform(m1_t);
+    }
+    //        throw std::runtime_error("not implemented yet");
+    //        // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
+    //        // будет отличаться от случая с гомографией
+    const int n_trials = 1500;//! TODO рассчитать
 
-        cv::Matx33d nt33 = cv::Matx33d::eye();
-        size_t count = m.size();
-        for(const cv::Vec2d& v : m){
+    const int n_samples = 8; //
+    uint64_t seed = 1;
 
-            nt33(0,2) += (v(0) / count);
-            nt33(1,2) += (v(1) / count);
+    int best_support = 0;
+    cv::Matx33d best_F;
+
+    std::vector<int> sample;
+    for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
+        phg::randomSample(sample, n_matches, n_samples, &seed);
+
+        cv::Vec2d ms0[n_samples];
+        cv::Vec2d ms1[n_samples];
+        for (int i = 0; i < n_samples; ++i) {
+            ms0[i] = m0_t[sample[i]];
+            ms1[i] = m1_t[sample[i]];
         }
 
-        return nt33;
+        cv::Matx33d F = estimateFMatrixDLT(ms0, ms1, n_samples);
 
-//        throw std::runtime_error("not implemented yet");
-    }
+        // denormalize
+        //            F = TODO
 
-    cv::Vec2d transformPoint(const cv::Vec2d &pt, const cv::Matx33d &T)
-    {
-        cv::Vec3d tmp = T * cv::Vec3d(pt[0], pt[1], 1.0);
+        F = F * TN0;
+        F = TN1.t() * F;
 
-        if (tmp[2] == 0) {
-            throw std::runtime_error("infinite point");
-        }
-
-        return cv::Vec2d(tmp[0] / tmp[2], tmp[1] / tmp[2]);
-    }
-
-    cv::Matx33d estimateFMatrixRANSAC(const std::vector<cv::Vec2d> &m0, const std::vector<cv::Vec2d> &m1, double threshold_px)
-    {
-        if (m0.size() != m1.size()) {
-            throw std::runtime_error("estimateFMatrixRANSAC: m0.size() != m1.size()");
-        }
-
-        const int n_matches = m0.size();
-
-        cv::Matx33d TN0 = getNormalizeTransform(m0);
-        cv::Matx33d TN1 = getNormalizeTransform(m1);
-
-        std::vector<cv::Vec2d> m0_t(n_matches);
-        std::vector<cv::Vec2d> m1_t(n_matches);
+        int support = 0;
         for (int i = 0; i < n_matches; ++i) {
-            m0_t[i] = transformPoint(m0[i], TN0);
-            m1_t[i] = transformPoint(m1[i], TN1);
-        }
-
-        {
-//             Проверьте лог: при повторной нормализации должно найтись почти единичное преобразование
-            getNormalizeTransform(m0_t);
-            getNormalizeTransform(m1_t);
-        }
-//        throw std::runtime_error("not implemented yet");
-//        // https://en.wikipedia.org/wiki/Random_sample_consensus#Parameters
-//        // будет отличаться от случая с гомографией
-        const int n_trials = 1500;//! TODO рассчитать
-
-        const int n_samples = 8; //
-        uint64_t seed = 1;
-
-        int best_support = 0;
-        cv::Matx33d best_F;
-
-        std::vector<int> sample;
-        for (int i_trial = 0; i_trial < n_trials; ++i_trial) {
-            phg::randomSample(sample, n_matches, n_samples, &seed);
-
-            cv::Vec2d ms0[n_samples];
-            cv::Vec2d ms1[n_samples];
-            for (int i = 0; i < n_samples; ++i) {
-                ms0[i] = m0_t[sample[i]];
-                ms1[i] = m1_t[sample[i]];
-            }
-
-            cv::Matx33d F = estimateFMatrixDLT(ms0, ms1, n_samples);
-
-            // denormalize
-//            F = TODO
-
-            int support = 0;
-            for (int i = 0; i < n_matches; ++i) {
-                if (phg::epipolarTest(m0[i], m1[i], F, threshold_px) && phg::epipolarTest(m1[i], m0[i], F, threshold_px))
-                {
-                    ++support;
-                }
-            }
-
-            if (support > best_support) {
-                best_support = support;
-                best_F = F;
-
-                std::cout << "estimateFMatrixRANSAC : support: " << best_support << "/" << n_matches << std::endl;
-                infoF(F);
-
-                if (best_support == n_matches) {
-                    break;
-                }
+            if (phg::epipolarTest(m0[i], m1[i], F, threshold_px) && phg::epipolarTest(m1[i], m0[i], F, threshold_px))
+            {
+                ++support;
             }
         }
 
-        std::cout << "estimateFMatrixRANSAC : best support: " << best_support << "/" << n_matches << std::endl;
+        if (support > best_support) {
+            best_support = support;
+            best_F = F;
 
-        if (best_support == 0) {
-            throw std::runtime_error("estimateFMatrixRANSAC : failed to estimate fundamental matrix");
+            std::cout << "estimateFMatrixRANSAC : support: " << best_support << "/" << n_matches << std::endl;
+            infoF(F);
+
+            if (best_support == n_matches) {
+                break;
+            }
         }
-
-        return best_F;
     }
+
+    std::cout << "estimateFMatrixRANSAC : best support: " << best_support << "/" << n_matches << std::endl;
+
+    if (best_support == 0) {
+        throw std::runtime_error("estimateFMatrixRANSAC : failed to estimate fundamental matrix");
+    }
+
+    return best_F;
+}
 
 }
 
@@ -191,12 +220,12 @@ cv::Matx33d phg::composeFMatrix(const cv::Matx34d &P0, const cv::Matx34d &P1)
     cv::Matx33d F;
 
 #define det4(a, b, c, d) \
-      ((a)(0) * (b)(1) - (a)(1) * (b)(0)) * ((c)(2) * (d)(3) - (c)(3) * (d)(2)) - \
-      ((a)(0) * (b)(2) - (a)(2) * (b)(0)) * ((c)(1) * (d)(3) - (c)(3) * (d)(1)) + \
-      ((a)(0) * (b)(3) - (a)(3) * (b)(0)) * ((c)(1) * (d)(2) - (c)(2) * (d)(1)) + \
-      ((a)(1) * (b)(2) - (a)(2) * (b)(1)) * ((c)(0) * (d)(3) - (c)(3) * (d)(0)) - \
-      ((a)(1) * (b)(3) - (a)(3) * (b)(1)) * ((c)(0) * (d)(2) - (c)(2) * (d)(0)) + \
-      ((a)(2) * (b)(3) - (a)(3) * (b)(2)) * ((c)(0) * (d)(1) - (c)(1) * (d)(0))
+    ((a)(0) * (b)(1) - (a)(1) * (b)(0)) * ((c)(2) * (d)(3) - (c)(3) * (d)(2)) - \
+    ((a)(0) * (b)(2) - (a)(2) * (b)(0)) * ((c)(1) * (d)(3) - (c)(3) * (d)(1)) + \
+    ((a)(0) * (b)(3) - (a)(3) * (b)(0)) * ((c)(1) * (d)(2) - (c)(2) * (d)(1)) + \
+    ((a)(1) * (b)(2) - (a)(2) * (b)(1)) * ((c)(0) * (d)(3) - (c)(3) * (d)(0)) - \
+    ((a)(1) * (b)(3) - (a)(3) * (b)(1)) * ((c)(0) * (d)(2) - (c)(2) * (d)(0)) + \
+    ((a)(2) * (b)(3) - (a)(3) * (b)(2)) * ((c)(0) * (d)(1) - (c)(1) * (d)(0))
 
     int i, j;
     for (j = 0; j < 3; j++)
